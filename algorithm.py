3f5342d3187f0cb4e09791a2c3fd025b0073b8ad
JohnSovero <u202115065@upc.edu.pe>
2022-11-17 19:43:24 -0500

    algorithm

--- algorithm.py
+++ algorithm.py
@@ -95,7 +95,6 @@ def paths(origen, destino):
     while node2 != -1:
         route_with_dfs.append(node2)
         node2 = dfspaths[node2]
-        a = 1
         
     #    return json.dumps({"bestpaths": bestpaths, "path1": path1, "path2": path2})
     return json.dumps({"bestpaths": route_with_dijkstra, "dfs": route_with_dfs})
