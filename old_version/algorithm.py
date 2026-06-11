37ccc790cfce4739367d2e5c6932b81593b84f6a
LuisFelipePoma <u202110902@upc.edu.pe>
2026-06-11 16:55:18 -0500

    pepe

--- old_version/algorithm.py
+++ old_version/algorithm.py
@@ -7,11 +7,12 @@ from haversine import haversine, Unit
 
 path = "https://raw.githubusercontent.com/LuisFelipePoma/DataSetForFlightRoutesApp/main/datasets/routes.json"
 
+
 def transformGraph():
     df = pd.read_json(path)
-    #print(df)
+    # print(df)
     rows = len(df)
-    #columns = len(df.columns)
+    # columns = len(df.columns)
     G = [[] for _ in range(3241)]
     id_origin = pd.Series(df._get_column_array(0))
     id_destination = pd.Series(df._get_column_array(2))
@@ -34,54 +35,58 @@ def transformGraph():
 
     return G
 
-def dijkstra(G, s):
-    n= len(G)
-    visited= [False]*n
-    path= [-1]*n
-    cost= [math.inf]*n
 
-    cost[s]= 0
-    pqueue= [(0, s)]
+def dijkstra(G, s):
+    n = len(G)
+    visited = [False] * n
+    path = [-1] * n
+    cost = [math.inf] * n
+
+    cost[s] = 0
+    pqueue = [(0, s)]
     while pqueue:
-        g, u= hq.heappop(pqueue)
+        g, u = hq.heappop(pqueue)
         if not visited[u]:
-            visited[u]= True
+            visited[u] = True
             for v, w in G[u]:
                 if not visited[v]:
-                    f= g + w
+                    f = g + w
                     if f < cost[v]:
-                        cost[v]= f
-                        path[v]= u
+                        cost[v] = f
+                        path[v] = u
                         hq.heappush(pqueue, (f, v))
 
     return path, cost
 
+
 def dfs(G, s):
-  n = len(G)
-  path = [-1]*n
-  visited = [False]*n
+    n = len(G)
+    path = [-1] * n
+    visited = [False] * n
 
-  def _dfs(u):
-    visited[u] = True
-    for v, w in G[u]:
-      if not visited[v]:
-        path[v] = u
-        _dfs(v)
+    def _dfs(u):
+        visited[u] = True
+        for v, w in G[u]:
+            if not visited[v]:
+                path[v] = u
+                _dfs(v)
 
-  _dfs(s)
-  return path
+    _dfs(s)
+    return path
+
 
 def prim(G, s):
     n = len(G)
-    visited = [False]*n
-    path = [-1]*n
-    cost = [math.inf]*n
+    visited = [False] * n
+    path = [-1] * n
+    cost = [math.inf] * n
 
     cost[s] = 0
     q = [(0, s)]
     while q:
         _, u = hq.heappop(q)
-        if visited[u]: continue
+        if visited[u]:
+            continue
         visited[u] = True
         for v, w in G[u]:
             if not visited[v] and w < cost[v]:
@@ -91,7 +96,9 @@ def prim(G, s):
 
     return path
 
+
 G = transformGraph()
+
 
 def get_route(path, destino):
     route = []
@@ -102,12 +109,13 @@ def get_route(path, destino):
         node = path[node]
     return route
 
+
 def paths(origen, destino):
     route_with_dijkstra = []
     route_with_dfs = []
     route_with_prim = []
 
-    bestpaths, _= dijkstra(G, origen)
+    bestpaths, _ = dijkstra(G, origen)
     dfspaths = dfs(G, origen)
     primpaths = prim(G, origen)
 
@@ -115,4 +123,6 @@ def paths(origen, destino):
     route_with_dfs = get_route(dfspaths, destino)
     route_with_prim = get_route(primpaths, destino)
 
-    return json.dumps({"djk": route_with_dijkstra, "dfs": route_with_dfs, "prim":route_with_prim})
+    return json.dumps(
+        {"djk": route_with_dijkstra, "dfs": route_with_dfs, "prim": route_with_prim}
+    )
